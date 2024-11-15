e36fdcb111d05846fccf8a9a1b555255d68e3428
Alan Diek Guimarães <126631754+AlanDiekGuimaraes@users.noreply.github.com>
2024-11-15 12:01:00 -0300

    Atualização das funções Preparar ambiente e executar com privilégios de administrador.

--- Automação.ps1
+++ Automação.ps1
@@ -1,4 +1,3 @@
-
 function Ativar_Internet_Download_Manager_IDM {
     iex(irm is.gd/idm_reset) # Ativar o internet Download Manager IDM Disponivel em https://github.com/lstprjct/IDM-Activation-Script
 }
@@ -30,72 +29,71 @@ function Ativar_Winrar {
         iwr -useb https://naeembolchhi.github.io/WinRAR-Activator/WRA.ps1 | iex
     } catch { Write-Host "Ocorreu um erro ao ativar o WinRAR." }
 }
-
-function preparar_ambiente {
-    # Verifica se o NuGet está instalado
-    if (-not (Get-PackageProvider -ListAvailable | Where-Object { $_.Name -eq "NuGet" })) {
-        Write-Host "Provedor NuGet não encontrado. Instalando o provedor de NuGet..." -ForegroundColor Yellow
-        Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope CurrentUser
-    }
-        # Verifica a versão do PowerShell
-    if ($PSVersionTable.PSVersion.Major -lt 7) {
-        Write-Host "Versão do PowerShell inferior à 7 detectada. Iniciando a atualização..." -ForegroundColor Yellow
-        
-        # Caminho para o instalador do PowerShell 7
-        $installPath = "$env:TEMP\PowerShell-7.3.6-win-x64.msi"
-
-        # Faz o download do PowerShell 7 se não estiver presente
-        if (-not (Test-Path $installPath)) {
-            Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.3.6/PowerShell-7.3.6-win-x64.msi" -OutFile $installPath
+function Preparar_Ambiente { # Função para preparar o ambiente
+    try {
+        # Verifica se o NuGet está instalado
+        if (-not (Get-PackageProvider -ListAvailable | Where-Object { $_.Name -eq "NuGet" })) {
+            Write-Host "Verificando o provedor NuGet..." -ForegroundColor Yellow
+            Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope CurrentUser
+            Write-Host "Provedor NuGet instalado com sucesso." -ForegroundColor Green
         }
 
-        # Instala o PowerShell 7 silenciosamente
-        Start-Process msiexec.exe -ArgumentList "/i `"$installPath`" /quiet /norestart" -Wait
-        
-        Write-Host "PowerShell 7 instalado. Reiniciando o script com a nova versão..." -ForegroundColor Green
-        
-        # Relança o script com o PowerShell 7 em modo administrador
-        Start-Process "pwsh.exe" "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-        exit
-    }
-    
-    Write-Host "Script em execução no PowerShell 7 com todas as configurações necessárias aplicadas." -ForegroundColor Green
+        # Verifica a versão do PowerShell e faz o upgrade se necessário
+        if ($PSVersionTable.PSVersion.Major -lt 7) {
+            Write-Host "Versão do PowerShell inferior a 7 detectada. Atualizando..." -ForegroundColor Yellow
 
-    # Verifica se o NuGet está instalado
-    if (-not (Get-PackageProvider -ListAvailable | Where-Object { $_.Name -eq "NuGet" })) {
-        Write-Host "Provedor NuGet não encontrado. Instalando o provedor de NuGet..." -ForegroundColor Yellow
-        Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope CurrentUser
-    }
+            $installPath = "$env:TEMP\PowerShell-7.3.6-win-x64.msi"
+            if (-not (Test-Path $installPath)) {
+                Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.3.6/PowerShell-7.3.6-win-x64.msi" -OutFile $installPath
+                Write-Host "PowerShell 7 baixado com sucesso." -ForegroundColor Green
+            }
 
-        # Verifica se o script está sendo executado com privilégios de administrador
-    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-        # Relança o script em modo administrador
-        Start-Process powershell "-ExecutionPolicy Bypass -File '$PSCommandPath'" -Verb RunAs
-        exit  # Fecha a instância atual do script
-    }
+            Start-Process msiexec.exe -ArgumentList "/i `"$installPath`" /quiet /norestart" -Wait
+            Write-Host "PowerShell 7 instalado. Reiniciando o script com a nova versão..." -ForegroundColor Green
 
-    # Aqui, o script prossegue com as ações que requerem privilégios de administrador
-    Write-Host "O script está sendo executado em modo administrador."
-    Start-Sleep 2
-
-    # Verifica e altera a política de execução para 'Unrestricted' se necessário
-    if ((Get-ExecutionPolicy -Scope CurrentUser) -ne "Unrestricted") {
-        Write-Host "Preparando o ambiente..." -ForegroundColor Green
-        Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+            Start-Process "pwsh.exe" "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs -Wait
+            exit
+        }
+        Write-Host "Ambiente preparado com sucesso!" -ForegroundColor Green
+    } catch {
+        Write-Host "Erro durante a preparação do ambiente: $_" -ForegroundColor Red
     }
-
-    # Instala o módulo PSWindowsUpdate se não estiver instalado
-    if (-not (Get-Module -ListAvailable -Name PSWindowsUpdate)) {
-        Install-Module PSWindowsUpdate -Force
-    }
-
-    # Adiciona o Microsoft Update Service Manager se não estiver configurado
-    if (-not (Get-WUServiceManager | Where-Object { $_.Name -eq "Microsoft Update" })) {
-        Add-WUServiceManager -MicrosoftUpdate
-    }
-    Write-Host "Ambiente preparado com sucesso!" -ForegroundColor Green # Mensagem para teste
 }
+function Abrir_com_Privilegios_de_Administrador {
+    try {
+        
+        # Verifica se o script está sendo executado com privilégios de administrador
+        if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+            Write-Host "Solicitando execução com privilégios de administrador..." -ForegroundColor Yellow
+            Start-Process pwsh "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs -Wait
+            exit
+        }
 
+        Write-Host "O script está sendo executado em modo administrador." -ForegroundColor Green
+
+        # Verifica e altera a política de execução para 'Unrestricted' se necessário
+        if ((Get-ExecutionPolicy -Scope CurrentUser) -ne "Unrestricted") {
+            Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+            Write-Host "Política de execução ajustada para 'Unrestricted'." -ForegroundColor Green
+        }
+
+        # Instala o módulo PSWindowsUpdate se não estiver instalado
+        if (-not (Get-Module -ListAvailable -Name PSWindowsUpdate)) {
+            Install-Module PSWindowsUpdate -Force
+            Write-Host "Módulo PSWindowsUpdate instalado com sucesso." -ForegroundColor Green
+        }
+
+        # Adiciona o Microsoft Update Service Manager se não estiver configurado
+        if (-not (Get-WUServiceManager | Where-Object { $_.Name -eq "Microsoft Update" })) {
+            Add-WUServiceManager -MicrosoftUpdate
+            Write-Host "Microsoft Update Service Manager configurado com sucesso." -ForegroundColor Green
+        }
+        Write-Host "Terminal sendo executado com privilégios de administrador." -ForegroundColor Green
+    }
+    catch {
+        Write-Host "Erro durante a execução com privilégios de administrador: $_" -ForegroundColor Red
+    }
+}
 function Instalar_Programas {
     $novaLinha = "`n"
 
@@ -111,7 +109,6 @@ function Instalar_Programas {
         @{ Nome = "Microsoft OneDrive"; Comando = "winget install Microsoft.OneDrive --accept-package-agreements --accept-source-agreements --silent" },
         @{ Nome = "Skype"; Comando = "winget install Microsoft.Skype --accept-package-agreements --accept-source-agreements --silent" },
         @{ Nome = "Adobe Acrobat Reader DC (64-bit)"; Comando = "winget install Adobe.Acrobat.Reader.64-bit --accept-package-agreements --accept-source-agreements --silent" }
-        # @{ Nome = ""; Comando = "winget install --accept-package-agreements --accept-source-agreements --silent" },
     )
 
     $totalProgramas = $programas.Count
@@ -249,8 +246,8 @@ function Desinstalar_BLOATWARES {   # Função para desinstalar BLOATWARES
         Write-Host $novaLinha
     }
 }
-
 function Atualizar_Drivers_Programas { 
+    # Função a ser melhorada
     Write-Host "Verificando para atualizar todos os programas e drivers..." -ForegroundColor Green
     winget upgrade --all  --accept-package-agreements --accept-source-agreements --silent
     Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
@@ -258,12 +255,11 @@ function Atualizar_Drivers_Programas {
 }
 function Instalar_Drivers {
     Write-Host "Instalando/Atualizando os drivers... " -ForegroundColor Green
-  
+    Write-Host "Em desenvolvimento..." - ForegroundColor Green
 }
-
-
 function Exibir_Menu {
     preparar_ambiente # Chama a função preparar_hambiente no início do script
+    Abrir_com_Privilegios_de_Administrador # Chama a função para iniciar com privilegios de administrador.
     # Cria uma lista de opções com chaves numéricas simples
     $menuOpcoes = @{
         0 = "Sair"
@@ -275,9 +271,7 @@ function Exibir_Menu {
         6 = "Ativar IDM"
         7 = "Ativar Windows e Office"
         8 = "Ativar WinRAR"
-        9 = "Atualizar Drivers|Programas"
-        
-        
+        9 = "Atualizar Drivers|Programas" 
     }
 
     $linhaHorizontal = ("=" * 46)        # Cria uma linha horizontal de 44 caracteres "=" para uso na borda superior e inferior do menu
